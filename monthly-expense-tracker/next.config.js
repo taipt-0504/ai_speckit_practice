62236a24df20786374f1ca948141e8b56e8d8ef4
phung.the.tai <phung.the.tai@sun-asterisk.com>
2026-04-28 10:37:22 +0700

    Fix error run project

--- monthly-expense-tracker/next.config.js
+++ monthly-expense-tracker/next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  typescript: {
-    strictNullChecks: true,
-    alwaysStrict: true,
-  },
-  eslint: {
-    dirs: ['app', 'components', 'lib', 'types'],
-  },
 };
 
 module.exports = nextConfig;
